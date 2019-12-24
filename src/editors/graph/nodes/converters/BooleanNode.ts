b358b811939bbb91184b1cfc3d0a347b753a899b
newcat <freddy.wagner@web.de>
2019-12-24 04:09:35 +0100

    New baklava

--- src/editors/graph/nodes/converters/BooleanNode.ts
+++ src/editors/graph/nodes/converters/BooleanNode.ts
@@ -1,10 +1,54 @@
-import { NodeBuilder } from "@baklavajs/core";
+import { Node } from "@baklavajs/core";
 
-export const BooleanNode = new NodeBuilder("BooleanNode")
-    .addInputInterface("Value 1", "NumberOption", 0, { type: "number" })
-    .addInputInterface("Value 2", "NumberOption", 0, { type: "number" })
-    .addInputInterface("Round", "CheckboxOption", false, { type: "boolean" })
-    .addInputInterface("Invert", "CheckboxOption", false, { type: "boolean" })
-    .addOption("Operation", "SelectOption", () => ({ selected: "==", items: ["==", ">", "<", ">=", "<="] }))
-    .addOutputInterface("Output", { type: "boolean" })
-    .build();
+const operations = [ "==", ">", "<", ">=", "<=" ];
+
+export class BooleanNode extends Node {
+
+    type = "BooleanNode";
+    name = this.type;
+
+    constructor() {
+        super();
+        this.addInputInterface("Value 1", "NumberOption", 0, { type: "number" });
+        this.addInputInterface("Value 2", "NumberOption", 0, { type: "number" });
+        this.addInputInterface("Round Values", "CheckboxOption", false, { type: "boolean" });
+        this.addInputInterface("Invert Output", "CheckboxOption", false, { type: "boolean" });
+        this.addOption("Operation", "SelectOption", "==", undefined, { items: operations });
+        this.addOutputInterface("Result", { type: "boolean" });
+    }
+
+    calculate() {
+
+        const val1 = this.getInterface("Value 1").value;
+        const val2 = this.getInterface("Value 2").value;
+        const useInt = this.getInterface("Round Values").value;
+        const invert = this.getInterface("Invert Output").value;
+        const operation = this.getOptionValue("Operation");
+
+        let result = false;
+        switch (operation) {
+            case "==":
+                result = useInt ? Math.round(val1) === Math.round(val2) : val1 === val2;
+                break;
+            case ">":
+                result = useInt ? Math.round(val1) > Math.round(val2) : val1 > val2;
+                break;
+            case "<":
+                result = useInt ? Math.round(val1) < Math.round(val2) : val1 < val2;
+                break;
+            case ">=":
+                result = useInt ? Math.round(val1) >= Math.round(val2) : val1 >= val2;
+                break;
+            case "<=":
+                result = useInt ? Math.round(val1) <= Math.round(val2) : val1 <= val2;
+                break;
+        }
+        if (invert) {
+            result = !result;
+        }
+
+        this.getInterface("Result").value = result;
+
+    }
+
+}
