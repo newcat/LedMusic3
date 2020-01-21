72fa2a641f0ed223ac833cdea9dff91b6545cee2
newcat <freddy.wagner@web.de>
2020-01-22 00:40:17 +0100

    Proper peak node

--- src/editors/graph/nodes/input/PeakNode.ts
+++ src/editors/graph/nodes/input/PeakNode.ts
@@ -8,16 +8,28 @@ export class PeakNode extends Node {
 
     public constructor() {
         super();
+        this.addInputInterface("Min Decibels", "NumberOption", -60, { type: "number", min: -200, max: 0 });
+        this.addInputInterface("Max Decibels", "NumberOption", 0, { type: "number", min: -200, max: 0 });
         this.addOption("Peak", "PeakOption", 0);
         this.addOutputInterface("Peak", { type: "number" });
     }
 
     public calculate() {
-        const data = new Uint8Array(globalProcessor.audioProcessor.analyserNode.fftSize);
-        globalProcessor.audioProcessor.analyserNode.getByteTimeDomainData(data);
-        let max = data[0];
-        for (const d of data) { if (d > max) { max = d; } }
-        const v = Math.min(1, Math.max(0, (max - 128) / 128));
+        const data = new Float32Array(globalProcessor.audioProcessor.analyserNode.fftSize);
+        globalProcessor.audioProcessor.analyserNode.getFloatTimeDomainData(data);
+
+        const sampleSize = Math.max(data.length, (globalProcessor.audioProcessor.audioContext.sampleRate / 1000) * 30);
+        let sum = 0;
+        for (let i = data.length - sampleSize; i < data.length; i++) {
+            sum += data[i] * data[i];
+        }
+        const rms = Math.sqrt(sum / (sampleSize / 2));
+        const db = 20 * Math.log10(rms);
+
+        const minDb = this.getInterface("Min Decibels").value;
+        const maxDb = this.getInterface("Max Decibels").value;
+        const v = Math.min(1, Math.max(0, (db - minDb) / (maxDb - minDb)));
+
         this.setOptionValue("Peak", v);
         this.getInterface("Peak").value = v;
     }
