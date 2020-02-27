f4b140c31fb4fe58a0d2186edc473c725bce3f62
newcat <freddy.wagner@web.de>
2020-02-27 22:38:02 +0100

    Fixed afterglow node

--- src/editors/graph/nodes/effects/AfterglowNode.ts
+++ src/editors/graph/nodes/effects/AfterglowNode.ts
@@ -13,7 +13,7 @@ export class AfterglowNode extends Node {
     public constructor() {
         super();
         this.addInputInterface("Input", undefined, [[0, 0, 0]], { type: "color_array" });
-        this.addInputInterface("Strength", "NumberOption", 0.05, { type: "number" });
+        this.addInputInterface("Strength", "NumberOption", 0.75, { type: "number" });
         this.addOutputInterface("Output", { type: "color_array" });
         this.initializeBuffer();
     }
@@ -26,9 +26,11 @@ export class AfterglowNode extends Node {
         const result = [];
         for (let i = 0; i < resolution; i++) {
             const a = i < input.length ? input[i] : ([0, 0, 0] as Color);
-            const b = i < this.buffer.length ? this.buffer[i] : ([0, 0, 0] as Color);
-            this.buffer[i] = mix(b, a, strength);
-            result[i] = blend(this.buffer[i], a, "lighten");
+            let b = i < this.buffer.length ? this.buffer[i] : ([0, 0, 0] as Color);
+            b = mix(b, [0, 0, 0], 1 - strength);
+            const c = blend(b, a, "lighten");
+            this.buffer[i] = c;
+            result[i] = c;
         }
 
         this.getInterface("Output").value = result;
