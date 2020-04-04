31dafa80d407e568634153b758941d6e509eb172
newcat <freddy.wagner@web.de>
2020-04-05 00:22:36 +0200

    UDP output

--- src/processing/globalProcessor.ts
+++ src/processing/globalProcessor.ts
@@ -4,6 +4,7 @@ import { TimelineProcessor } from "./timelineProcessor";
 import { globalState } from "@/entities/globalState";
 import { StandardEvent } from "@/lokumjs";
 import { Color } from "@/editors/graph/colors";
+import { Socket, createSocket } from "dgram";
 
 class GlobalProcessor {
 
@@ -18,10 +19,17 @@ class GlobalProcessor {
     };
 
     private timer: any = null;
+    private socket?: Socket;
+    private sendTo = -1;
 
     public constructor() {
         (window as any).globalProcessor = this;
         globalState.events.initialized.subscribe(this, () => this.initialize());
+        this.socket = createSocket("udp4");
+        this.socket.on("message", (msg, info) => {
+            this.sendTo = info.port;
+        });
+        this.socket.bind(41234);
     }
 
     public initialize() {
@@ -53,6 +61,16 @@ class GlobalProcessor {
         this.audioProcessor.updatePosition();
         this.timelineProcessor.process(globalState.position);
         this.events.tick.emit();
+        if (this.socket && this.sendTo > 0) {
+            const arr = new Uint8Array(this.globalPreview.length * 3 + 1);
+            arr[0] = this.globalPreview.length;
+            for (let i = 0; i < this.globalPreview.length; i++) {
+                arr[3 * i + 1] = this.globalPreview[i][0];
+                arr[3 * i + 2] = this.globalPreview[i][1];
+                arr[3 * i + 3] = this.globalPreview[i][2];
+            }
+            this.socket.send(arr, this.sendTo);
+        }
     }
 
 }
