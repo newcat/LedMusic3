38af10f058c7edcd69adaddb570bef0748a37229
newcat <freddy.wagner@web.de>
2020-05-06 02:28:51 +0200

    Fix LFO node

--- src/editors/graph/nodes/input/LfoNode.ts
+++ src/editors/graph/nodes/input/LfoNode.ts
@@ -23,15 +23,16 @@ export class LfoNode extends Node {
 
     private functions: Record<string, (x: number) => number> = {
         Sine: (x: number) => Math.sin(Math.PI * 2 * x),
-        Triangle: (x: number) => 4 * (x - 0.5 * Math.floor(2 * x + 0.5)) * Math.pow(-1, Math.floor(2 * x + 0.5)),
+        Triangle: (x: number) => 4 * Math.abs(x - Math.floor(x + 0.5)) - 1,
         Sawtooth: (x: number) => 2 * x - 1,
-        Square: (x: number) => x < 0.5 ? -1 : 1
+        Square: (x: number) => Math.sign(Math.sin(Math.PI * 2 * x))
     };
 
     public constructor() {
         super();
         this.addInputInterface("Min", "NumberOption", 0, { type: "number" });
         this.addInputInterface("Max", "NumberOption", 1, { type: "number" });
+        this.addInputInterface("Offset", "SliderOption", 0, { type: "number", min: 0, max: 1 });
         this.addInputInterface("Invert", "CheckboxOption", false, { type: "boolean" });
         this.addOption("Rate", "SelectOption", "1/2 Beat", undefined, { items: this.rates.map((r) => r.text) });
         this.addOption("Shape", "SelectOption", "Sine", undefined, { items: this.shapes });
@@ -42,13 +43,14 @@ export class LfoNode extends Node {
 
         const min = this.getInterface("Min").value;
         const max = this.getInterface("Max").value;
+        const offset = this.getInterface("Offset").value;
         const invert = this.getInterface("Invert").value;
 
         const rate = this.rates.find((r) => r.text === this.getOptionValue("Rate"))?.value ?? 1;
         const shape = this.getOptionValue("Shape");
 
         const f = this.functions[shape] ?? ((v: number) => v);
-        const x = (globalState.position % rate) / rate;
+        const x = (globalState.position % rate) / rate + offset;
         const rawValue = invert ? -f(x) : f(x);
         const value = min + (rawValue + 1) * 0.5 * (max - min);
         this.getInterface("Value").value = value;
